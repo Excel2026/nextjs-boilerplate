d5c866ec1792d8af591d9cfb4d7416c7c59a3929
Excel2026 <dan.hill4@aol.com>
2025-10-11 09:42:18 -0400

    checkout/route

--- app/api/checkout/app/api/app/api/checkout/route.ts
+++ app/api/checkout/app/api/app/api/checkout/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
 import Stripe from "stripe";
-
+export const runtime = 'nodejs';
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: "2024-06-20",
 });
